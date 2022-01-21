bc07b36770368543e4cfd194abf1e5a13a990d09
Monish-P <121910311046@gitam.in>
2022-01-21 16:27:53 +0530

    commited

--- whatsappbot/settings.py
+++ whatsappbot/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bot'
-    'storages'
+    'bot',
+    'storages',
 ]
 
 MIDDLEWARE = [
